19147effeb238e230e71b36af6bc0965e8a84336
Florian-EagleGenomics <florian.reisinger@eaglegenomics.com>
2016-09-12 14:07:34 +0100

    Remove the version specification from poise-python
    The defined version clashes with the version specified by cloudcli.
    Since we don't have control over cloudcli and we probably don't need
    the latest poise-python version.

--- metadata.rb
+++ metadata.rb
@@ -4,12 +4,12 @@ maintainer_email 'chef@eaglegenomics.com'
 license 'Apache v2.0'
 description 'Installs/Configures graphlan'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.1.1'
+version '0.1.2'
 
 source_url 'https://github.com/EagleGenomics-cookbooks/graphlan'
 issues_url 'https://github.com/EagleGenomics-cookbooks/graphlan/issues'
 
 depends 'apt'
 depends 'magic_shell'
-depends 'poise-python', '~> 1.4.0'
-depends 'mercurial', '~> 2.0.4'
+depends 'poise-python'
+depends 'mercurial', '~> 2.0'
