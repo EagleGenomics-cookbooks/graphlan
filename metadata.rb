bc2091ea695fa91a644d66665a442007bcd85ac5
GreetDB <grdbaets@gmail.com>
2017-11-30 10:45:34 +0000

    specify chefdk=1.6.11-1 otherwise rspec test on mercurial fail

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'chef@eaglegenomics.com'
 license 'Apache v2.0'
 description 'Installs/Configures graphlan'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.1.2'
+version '0.1.3'
 
 source_url 'https://github.com/EagleGenomics-cookbooks/graphlan'
 issues_url 'https://github.com/EagleGenomics-cookbooks/graphlan/issues'
